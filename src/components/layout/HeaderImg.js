e34c0e765fa3a6d9dae68a4739a815625fc05e0b
njayman <najishm828282@gmail.com>
2020-06-27 20:33:09 +0600

    medibee setup

--- src/components/layout/HeaderImg.js
+++ src/components/layout/HeaderImg.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import hexagon from '../../images/hexagon.jpg'
+import hexagon from '../../images/hexagon2.png'
 class HeaderImg extends Component {
     render() {
         return (
