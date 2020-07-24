d845dc1e1f4b49a19b294a75141122b5b1dfd279
zahid0033 <zahidhossain0033@gmail.com>
2020-07-24 22:12:41 +0600

    navbar modified

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -79,6 +79,24 @@ class Landing extends Component {
           {/*greetings*/}
           <div className="container" style={{padding: "25px 0"}}>
             <h3 className="center-align" > <b className=" red-text text-darken-1" style={{wordSpacing:"10px"}}>Greetings From Doctors</b> </h3>
+
+            <div className="row">
+              <div className="col s12 m7">
+                <div className="card">
+                  <div className="card-image center-align">
+                    <img src={logo} alt="MEDIBEE" style={{width:"100px",height:"100px"}}/>
+                      {/*<span className="card-title">Card Title</span>*/}
+                  </div>
+                  <div className="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                      I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div className="card-action">
+                    <a href="#">This is a link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           {/*greetings end*/}
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -31,11 +31,11 @@ class Navbar extends Component {
                     <li><LinkStyled to="/liveclass">Live Classroom</LinkStyled></li>
                     <li><LinkStyled to="/course">Courses</LinkStyled></li>
                     <li><LinkStyled to="/course">Mentors</LinkStyled></li>
-                    <li><LinkStyled to="/training">Training</LinkStyled></li>
+                    {/*<li><LinkStyled to="/training">Training</LinkStyled></li>*/}
                     {this.props.auth.isAuthenticated ? <li><LinkStyled className="grey" to="#">{this.props.auth.user.name}</LinkStyled></li> :
                         <li>
                             <LinkStyled to="/mentor">
-                                Mentors Here!
+                                Sign Up
                                 </LinkStyled>
                         </li>
                     }
@@ -57,11 +57,11 @@ class Navbar extends Component {
                                 <li><LinkStyled to="/liveClass">Live Classroom</LinkStyled></li>
                                 <li><LinkStyled to="/course">Courses</LinkStyled></li>
                                 <li><LinkStyled to="/course">Mentors</LinkStyled></li>
-                                <li><LinkStyled to="/training">Training</LinkStyled></li>
+                                {/*<li><LinkStyled to="/training">Training</LinkStyled></li>*/}
                                 {this.props.auth.isAuthenticated ? <li><UserIcon /></li> :
                                     <li>
                                         <Link className="teal darken-1" to="/mentor">
-                                            Mentors Here!
+                                            Sign Up
                                         </Link>
                                     </li>
                                 }
