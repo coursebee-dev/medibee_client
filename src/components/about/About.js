c8a3937fb6c720c47bd09793e09d8f922c4234e0
njayman <najishm828282@gmail.com>
2020-08-05 00:13:07 +0600

    about info updates

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -34,12 +34,27 @@ class About extends Component {
                             About<b> Medibee</b>
                         </h4>
                         <blockquote>
+                            Join "Medibee" Team
                             <p>
-                                Medibee একটি অনলাইন শিক্ষামূলক প্ল্যাটফর্ম। আমাদের মূল লক্ষ্য হল একটি ইউনিফাইড প্ল্যাটফর্মে শিক্ষার্থী এবং প্রফেশনালদের একত্রিত করা।শিক্ষার্থীরা প্রায়শই বিভিন্ন সমস্যায় আটকে যায় যা তারা সবসময় নিজেরা সামলাতে পারে না। যথাযথ গাইড্লাইন না পেয়ে, তারা অনেক সময় ভুল সিদ্ধান্ত নেয় যা তারা ভবিষ্যতে অনুশোচনা করে।
-                    <br /><br />Medibee এমন একটি প্ল্যাটফর্ম যেখানে তারা প্রফেশনালদের কাছ থেকে পরামর্শ নিতে পারবে এবং তাদের অভিজ্ঞতা share করতে পারবে।
-                    এখানে, গ্রুপের সদস্যরা তাদের ক্রিয়েটিভ চিন্তাভাবনা, প্রতিদিনের সমস্যাগুলি, ক্যারিয়ার পরামর্শ, নির্দেশিকা ইত্যাদি share করবে, যা অন্যান্য সদস্যদের সহায়তা করবে। বীজগণিত, ফ্লুইড মেকানিক্স,অ্যাকাউন্টিং, ফটোগ্রাফি, কোডিং, অর্থনীতি, সাহিত্য সহ সবকিছু এই প্ল্যাটফর্মের আওতায় আসবে।
-                    এই গ্রুপে আমরা বিভিন্ন বিষয়ে লাইভ সেশন এবং ওয়েবিনারের ব্যবস্থা করব যেখানে শিক্ষার্থীরা পেশাদারদের এবং বিশেষজ্ঞদের সাথে লাইভ ইনটারেকশন করতে পারে। এই কমিউনিটির মাধ্যমে Medibee হবে বাংলাদেশের বৃহত্তম অনলাইন শিক্ষামূলক প্ল্যাটফর্মে।
-                </p>
+
+                                Medibee is medical education related Organization has been working relentlessly to improve the academic & co- curricular skills of medical students & doctors across the country and abroad.</p>
+
+                            <p>The core concept of the group is to connect the medical students to renowned teachers and professionals working in different medical colleges, hospitals, government and private organizations in Bangladesh and abroad. We try to develop academic lectures, video lectures, tutorials for all & easily accessible from every corner of the country. We also arrange clinical workshops, skill development programs health awareness campaign, career seminar & so on to help medical students & doctors to improve their skills & serve their patients in a better way. We also arrange live session on various contemporary issues. Medical knowledge dissemination in remote areas is a very essential task for us as a leading medical education organization.</p>
+                            <b>Slogan:</b>
+                            <b>Vision:</b>
+                            <p>To create and foster a medical learning community to provide learners with knowledge, skills and attitudes to successfully overcome current and future challenges in health care.</p>
+                            <b>Mission:</b>
+                            <p>The mission of MediBee is to educate medical students, physicians and other health professionals from demographically diverse backgrounds to meet future primary and specialty health care needs; foster medical research that leads to improvement of clinical and public health; and provide a broad range of patient services.</p>
+                            <b>Projects & programmes:</b>
+                                Online and offline class lectures
+                                Contemporary knowledge sharing sessions
+                                Webinar and seminars organize
+                                Higher education abroad: Opportunism and challenges sharing.
+                                Professional and career discussion
+                                Medical research and publications workshop
+
+                                <b>Our Core values:</b>
+                            <p>Medical excellence, Quality, Innovation, Research and Development, Accuracy, teambuilding.</p>
                         </blockquote>
                     </div>
                 </div>
