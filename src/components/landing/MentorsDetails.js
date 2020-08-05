371891a741c436ac388de9778d74c24c56d52ffb
zahid0033 <zahidhossain0033@gmail.com>
2020-08-05 12:42:07 +0600

    changes 11

--- src/components/landing/MentorsDetails.js
+++ src/components/landing/MentorsDetails.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux'
-<<<<<<< HEAD
-import Axios from 'axios'
-import Breadcrumbs from "../layout/Breadcrumbs";
-=======
 import axios from 'axios';
->>>>>>> dc009916006d12f2b2d067c29d5aa6b63e8248f4
 
 export const MentorsDetails = ({ match, auth }) => {
     const [details, setDetails] = useState({})
@@ -19,13 +14,6 @@ export const MentorsDetails = ({ match, auth }) => {
             console.log(error)
         }
 
-<<<<<<< HEAD
-    useEffect( () => {
-        const getMentorDetail = async () => {
-            const { data } = await Axios.get(`/api/mentors/${match.params.id}`);
-            setDetails(data)
-        };
-=======
     }
 
     const getMentorDetail = async () => {
@@ -38,53 +26,12 @@ export const MentorsDetails = ({ match, auth }) => {
     }
     useEffect(() => {
         getSubject()
->>>>>>> dc009916006d12f2b2d067c29d5aa6b63e8248f4
         getMentorDetail()
-    }, []);
-
+    }, [])
     useEffect(() => {
         console.log(details)
-    }, [details]);
+    }, [details])
     return (
-<<<<<<< HEAD
-        <>
-            <Breadcrumbs title="Mentors Details" description="Mentor Details" />
-            <div className="container">
-                {auth.isAuthenticated ? (
-                    <div className="section">
-                        <h2 className="center-align">{details.name}</h2>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <h5><b>Email : </b>{details.email}</h5>
-                                <h5><b>Medical College : </b>{details.medicalcollege}</h5>
-                                <h5><b>Position : </b>{details.position}</h5>
-                                <h5><b>Session : </b>{details.session}</h5>
-                                <h5><b>Mobile No : </b>{details.mobileNo}</h5>
-                            </div>
-                        </div>
-
-                        <div>
-                            {details.preferred_topic?.map((ptopic, id) => (
-                                <li key={id}>{ptopic}</li>
-                            ))}
-                        </div>
-                        <div>
-                            {details.subject_level?.map((slevel, id) => (
-                                <li key={id}>{slevel}</li>
-                            ))}
-                        </div>
-                        <div>
-                            {details.subjects?.map((sub, id) => (
-                                <li key={id}>{sub.subject}</li>
-                            ))}
-                        </div>
-                        <div>
-                            {details.subjects?.map((sub, id) => (
-                                <li key={id}>{sub.subcategory}</li>
-                            ))}
-                        </div>
-
-=======
         <div className="container">
             {auth.isAuthenticated ? (
                 <div className="section">
@@ -123,9 +70,9 @@ export const MentorsDetails = ({ match, auth }) => {
                         {details.subcategory?.map((subcat, id) => (
                             <span key={id}>{subcat}</span>
                         ))}
->>>>>>> dc009916006d12f2b2d067c29d5aa6b63e8248f4
                     </div>
-                ) : (
+                </div>
+            ) : (
                     <div className="container">
                         <div className="section" style={{ display: "flex", flexDirection: "column", justifyContent: "center", }}>
                             <h1>You must login to view this page.</h1>
@@ -133,14 +80,9 @@ export const MentorsDetails = ({ match, auth }) => {
                         </div>
                     </div>
                 )}
-<<<<<<< HEAD
-            </div>
-        </>
-=======
         </div>
->>>>>>> dc009916006d12f2b2d067c29d5aa6b63e8248f4
     )
-};
+}
 
 const mapStateToProps = state => ({
     auth: state.auth,
